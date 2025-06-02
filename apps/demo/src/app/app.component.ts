dc34ab2daf7d731d30d8cb7ec81c331dad21865c
Masha <masha@MacBook-Air.local>
2025-06-02 18:04:04 +0200

    angular material setup

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent],
+  imports: [
+    NxWelcomeComponent,
+    CommonModule,
+    MatButtonModule
+  ],
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,6 +1,15 @@
+import '@angular/localize/init';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+providers: [
+  importProvidersFrom(BrowserAnimationsModule),
+  ...(appConfig.providers ?? []),
+]
+}).catch((err) => console.error(err));
